bdf76624bbc9d32949c4438894fd58ef81d8fe4e
jodyfleck <jody_fleck@hotmail.com>
2023-12-16 13:28:04 -0500

    more installs

--- eamdatavalidation.py
+++ eamdatavalidation.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np 
 import plotly as px
+import matplotlib
 #import pandas_profiling
 #from streamlit_pandas_profiling import st_profile_report 
 #from pycaret.regression import setup, compare_models, pull, save_model, load_model
